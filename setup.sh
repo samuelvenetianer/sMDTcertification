e466b62f8d474a2333df54fbc25aeb336a773ee6
Samuel Hugo Venetianer <svenetia@pcatum04.dyndns.cern.ch>
2025-08-01 22:19:26 +0200

    set analysis home directory

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 # Create shortcut for important paths
-echo 'Setting results path...'
+echo 'Setting results directory...'
 Results_Dir=/net/ustc_01/users/lich/muon/results/
 echo $Results_Dir
+
+echo 'Setting analysis directory...'
+Analysis_Dir=/home/svenetia/sMDTcertification/online
+echo $Analysis_Dir
